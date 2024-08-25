c0c760d84acd34ed37d658c45bae6f403cd86ca7
jeongeungyeong <jeongeung1124@gmail.com>
2024-08-26 02:57:17 +0900

    feat : 회원등급 속성 추가

--- src/main/java/wanted/media/user/dto/UserInfoDto.java
+++ src/main/java/wanted/media/user/dto/UserInfoDto.java
@@ -3,6 +3,7 @@ package wanted.media.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import wanted.media.user.domain.Grade;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -10,4 +11,5 @@ import lombok.NoArgsConstructor;
 public class UserInfoDto {
     private String account;
     private String email;
+    private Grade grade; // 현재 회원등급
 }
