d308048aea2cc33bc5b499d475e5862bea9bfe95
Jinhui <jda0526@naver.com>
2024-08-27 00:00:24 +0900

    refactor : dev 브랜치와 충돌 해결

--- src/main/java/wanted/media/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/wanted/media/exception/handler/GlobalExceptionHandler.java
@@ -4,9 +4,8 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import wanted.media.exception.ErrorCode;
+import wanted.media.exception.CustomException;
 import wanted.media.exception.ErrorResponse;
-import wanted.media.exception.PostListCustomException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,17 +15,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(400, e.getMessage()));
     }
-    
-    @ExceptionHandler(PostListCustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(PostListCustomException ex) {
-        ErrorCode errorCode = ex.getErrorCode();
-        ErrorResponse response = new ErrorResponse(errorCode);
-        return new ResponseEntity<>(response, errorCode.getStatus());
-    }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
-        ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<>(response, ErrorCode.INTERNAL_SERVER_ERROR.getStatus());
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponse> handleCustomException(final CustomException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus().value())
+                .body(new ErrorResponse(e.getErrorCode().getStatus().value(), e.getCustomMessage()));
     }
 }
