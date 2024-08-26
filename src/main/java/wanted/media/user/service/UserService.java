256a71093b4ac275787ace49b6829285c77350fc
Jinhui <jda0526@naver.com>
2024-08-27 00:11:56 +0900

    refactor : dev 브랜치와 충돌 해결

--- src/main/java/wanted/media/user/service/UserService.java
+++ src/main/java/wanted/media/user/service/UserService.java
@@ -7,18 +7,22 @@ import org.springframework.stereotype.Service;
 import wanted.media.user.config.TokenProvider;
 import wanted.media.user.domain.Code;
 import wanted.media.user.domain.Grade;
+import wanted.media.user.domain.Token;
 import wanted.media.user.domain.User;
 import wanted.media.user.dto.*;
 import wanted.media.user.repository.CodeRepository;
+import wanted.media.user.repository.TokenRepository;
 import wanted.media.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class UserService {
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final TokenProvider tokenProvider;
     private final CodeRepository codeRepository;
     private final BCryptPasswordEncoder passwordEncoder;
@@ -30,7 +34,16 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("account나 password를 다시 확인해주세요."));
         if (!requestDto.getPassword().equals(user.getPassword())) // password 암호화 저장시 변경하기
             throw new IllegalArgumentException("account나 password를 다시 확인해주세요.");
-        return new UserLoginResponseDto(user.getUserId(), tokenProvider.makeToken(requestDto.getAccount()));
+
+        Optional<Token> refreshToken = tokenRepository.findByUserId(user.getUserId()); // 리프레시 토큰 있는지 확인
+        String newRefreshToken = tokenProvider.makeToken(requestDto.getAccount(), "refresh"); // 새 리프레시 토큰
+        if (refreshToken.isPresent()) { // 리프레시 토큰 있을 경우
+            tokenRepository.save(refreshToken.get().updateToken(newRefreshToken)); // 새 토큰으로 업데이트
+        } else { // 리프레시 토큰 없을 경우
+            tokenRepository.save(new Token(newRefreshToken, user)); // 새 토큰 저장
+        }
+
+        return new UserLoginResponseDto(user.getUserId(), tokenProvider.makeToken(requestDto.getAccount(), "access"));
     }
 
     //회원가입
